e2ecaba7a5cd32e073c92e135ecad49a346cad43
Naim Mustafa <naimmetinmustafa@gmail.com>
2019-07-21 19:18:51 +0100

    Updates

---
+++ precache-manifest.84224edcaa906f36e77a8c89c534481e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fdb878b0e55c882552fa84f2262110",
+    "url": "/buenocynara/cynarabueno.github.io/index.html"
+  },
+  {
+    "revision": "fae32b85484dc636b283",
+    "url": "/buenocynara/cynarabueno.github.io/static/css/main.0d818f56.chunk.css"
+  },
+  {
+    "revision": "6426adf868d35940f2e2",
+    "url": "/buenocynara/cynarabueno.github.io/static/js/2.58a8ff07.chunk.js"
+  },
+  {
+    "revision": "fae32b85484dc636b283",
+    "url": "/buenocynara/cynarabueno.github.io/static/js/main.23c3130c.chunk.js"
+  },
+  {
+    "revision": "355c6141722f44c93f33",
+    "url": "/buenocynara/cynarabueno.github.io/static/js/runtime~main.393c2f2c.js"
+  },
+  {
+    "revision": "36d5a406da6d2170a9414e8f012a3c3f",
+    "url": "/buenocynara/cynarabueno.github.io/static/media/logo.36d5a406.png"
+  },
+  {
+    "revision": "c17ad7b1d6e4999023a38cdbae6a9dd1",
+    "url": "/buenocynara/cynarabueno.github.io/static/media/profile.c17ad7b1.png"
+  },
+  {
+    "revision": "550eb819e01dce5fab2edcb691bef832",
+    "url": "/buenocynara/cynarabueno.github.io/static/media/single_logo.550eb819.png"
+  }
+]);
